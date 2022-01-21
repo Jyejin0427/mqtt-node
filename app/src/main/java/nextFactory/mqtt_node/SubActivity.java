25b6c6913c0153088e81b33fd7f9143bcb23c380
지인 <sonia9.shin@gmail.com>
2022-01-21 16:59:25 +0900

    지인) subActivity 생성2
    서브 액티비티 생성 publish로 갈건지 subscribe로 갈건지 결정하는 버튼
    (기존에는 connect 버튼 누르면 구독 액티비티로 이동했는데 이제 connect는 진짜 연결만)

---
+++ app/src/main/java/nextFactory/mqtt_node/SubActivity.java
@@ -0,0 +1,30 @@
+package nextFactory.mqtt_node;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+    Button toPublish, toSubscribe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        toPublish = findViewById(R.id.toPublish);
+        toSubscribe = findViewById(R.id.toSubscribe);
+
+        toPublish.setOnClickListener(view->{
+            Intent pub = new Intent(SubActivity.this, PublishActivity.class);
+            startActivity(pub);
+        });
+        toSubscribe.setOnClickListener(view->{
+            Intent sub = new Intent(SubActivity.this, SubscribeActivity.class);
+            startActivity(sub);
+        });
+    }
+}
